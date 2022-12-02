fdea09dcbb5d632bb1e7095c6856f389630c3d8f
sbkim <victory289@naver.com>
2022-12-03 01:42:56 +0900

    JPA

--- src/main/java/hellospring/domain/Member.java
+++ src/main/java/hellospring/domain/Member.java
@@ -1,8 +1,17 @@
 package hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
+
+  @Id
+  @GeneratedValue (strategy = GenerationType.IDENTITY) // ! @GeneratedValue => DB 가 알아서 IDENTITY를 생성하도록 만든다.
   private Long id; // 데이터를 구분하기 위해서 시스템에 저장하는 id임 // 고객을 구분하기 위한 id가 아님.
+
+//  @Column(name = "NAME") // - EXAMPLE => JPA > Anotation > Column => DB의 "NAME" Column에 자동 매핑해준다.
   private String name;
+
 
   public Long getId() {
     return id;
--- src/main/java/hellospring/service/MemberService.java
+++ src/main/java/hellospring/service/MemberService.java
@@ -5,6 +5,7 @@ import hellospring.repository.MemberRepository;
 import hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +16,9 @@ import java.util.Optional;
 // repository 코드는 개발자스럽게(?) 데이터 저장, 읽기, 수정, 삭제 등의 기능 중심으로 코드를 구성한다.
 
 //@Service
+//@Transactional // <-- 회원가입할 때만, JPA를 쓸거라서, 클래스 자체에 Anotation걸어두지 않아도 된다.
+// - @Transactional: 데이터를 저장하고 변경할 때 항상 필요
+// - @Transactional: Spring with JPA를 사용하려면, 반드시 붙여줘야한다. (클래스에 어노테이션 또는.... 메서드위에 어노테이션으로 붙이기)
 public class MemberService {
 
   // !!!! new Instance Ver.
@@ -34,9 +38,11 @@ public class MemberService {
   /*
   * 회원 가입
   * */
+  // ! Spring with JPA를 사용하려면, 반드시 붙여줘야한다. (클래스에 어노테이션 또는.... 메서드위에 어노테이션으로 붙이기)
+  // !  JPA는 모든 데이터변경이 실행될 때, Transactional 내부에서 실행되어야 한다.
+  @Transactional
   public Long join(Member member) {
-    // condition : 동명이인 회원가입 불가.
-    validateDuplicateMember(member);
+    validateDuplicateMember(member); // condition : 동명이인 회원가입 불가.
     memberRepository.save(member);
     return member.getId();
   }
