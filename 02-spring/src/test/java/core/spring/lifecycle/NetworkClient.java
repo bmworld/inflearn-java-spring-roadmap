0a50483295fcb553599e843b6f2d559092543dfc
sbkim <victory289@naver.com>
2023-06-24 18:01:12 +0900

    02-SPRING- Bean Life Cycle - 스프링 기본 Interface 활용하기 (* 비추천)
    - Srping Interface: InitializingBea, DisposableBean
    
    - Spring 초창기에 사용한 방법이고,
      더 나은 방법이 있으므로, 비추천 by 김영한 강사님)

--- 02-spring/src/test/java/core/spring/lifecycle/NetworkClient.java
+++ 02-spring/src/test/java/core/spring/lifecycle/NetworkClient.java
@@ -1,12 +1,14 @@
 package core.spring.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
   private String url;
 
   public NetworkClient() {
     System.out.println("생성자 호출, url = " + url);
-    connect();
-    call("초기화 연결 메시지");
+
   }
 
   public void setUrl(String url) {
@@ -29,4 +31,20 @@ public class NetworkClient {
     System.out.println("disconnect = " + url);
   }
 
+
+  /**
+   * 의존관계 주입이 끝난 경우 호출되는 메서드
+   */
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    System.out.println("NetworkClient.afterPropertiesSet");
+    connect();
+    call("초기화 연결 메시지");
+  }
+
+  @Override
+  public void destroy() throws Exception {
+    System.out.println("NetworkClient.destroy");
+    disconnect();
+  }
 }
