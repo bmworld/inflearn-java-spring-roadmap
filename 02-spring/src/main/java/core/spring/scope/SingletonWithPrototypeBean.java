6f6b7893539e8fb82c7e2042fb0579adf3a70aee
sbkim <victory289@naver.com>
2023-06-25 02:50:16 +0900

    02-SPRING- Bean Scope - Singleton Bean & Prototype Bean => `Provider` ( 함께 사용 시, 문제 해결방법 )
    - ObjectFactory, ObjectProvider의 문제점
     : Spring에 의존적이다.
    
    - 이것의 해결방법은, 다음 커밋에 등장한다.

--- 02-spring/src/main/java/core/spring/scope/SingletonWithPrototypeBean.java
+++ 02-spring/src/main/java/core/spring/scope/SingletonWithPrototypeBean.java
@@ -1,12 +1,15 @@
 package core.spring.scope;
 
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -21,7 +24,26 @@ public class SingletonWithPrototypeBean {
 
   private final PrototypeBean prototypeBean; // 생성 시점에, 주입
 
+  /**
+   * ApplicationContext를 Singleton에서 주입받는 것은, 좋은 설계가 아님
+   */
+  @Autowired
+  private ApplicationContext ac;
 
+
+
+  /**
+   * @ObjectFactory ApplicationContext 내부의 Bean의 `찾는 기능` 만 제공(기능 단순함) <br/>
+   *  - 별도의 라이브러리 필요없음<br/>
+   *  - Spring에 의존적<br/>
+   *  - {@link ObjectFactory#getObject()}
+   * @ObjectProvider {@link ObjectFactory}기능 + 추가적인 기능 <br/>
+   *                  따라서, ApplicationContext 주입 없이 (의존관계를 끊고), Prototype을 의도대로 사용 가능 <br/>
+   *
+   *
+   */
+  @Autowired
+  private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
   // Constructor 시점에 Spring Container 에서 prototype Bean 요청 => Prototype Bean이 이 시점에 생성됨
   @Autowired
@@ -29,6 +51,20 @@ public class SingletonWithPrototypeBean {
     this.prototypeBean = prototypeBean;
   }
 
+  public int intentionalLogic() {
+    PrototypeBean intendedPrototypeBean = prototypeBeanProvider.getObject();
+    intendedPrototypeBean.addCount();
+    return intendedPrototypeBean.getCount();
+  }
+
+  public PrototypeBean getPrototypeBean() {
+    return prototypeBean;
+  }
+
+
+  public PrototypeBean getPrototypeBeanByObjectProvider() {
+    return prototypeBeanProvider.getObject();
+  }
   public int unintentionalLogic() {
     // Singleton에서 Prototype 로직의 의도와 달리,  `신규생성되지 않음`
     // 최초 주입받은 prototype 재사용된다.
@@ -38,11 +74,15 @@ public class SingletonWithPrototypeBean {
   }
 
 
-  public int intentionalLogicButBadCode(ApplicationContext ac) {
-    // * 어떤 방식이든, ApplicationContext를 singleton에서 주입받는 것은, 좋은 설계가 아님.
+  public Map<String, Object> intentionalLogicButBadCode() {
+
     PrototypeBean ptBean = ac.getBean(PrototypeBean.class);
     ptBean.addCount();
-    return ptBean.getCount();
+    int count = ptBean.getCount();
+    Map<String, Object> resultMap = new HashMap<>();
+    resultMap.put("count", count);
+    resultMap.put("bean", ptBean);
+    return resultMap;
 
 
   }
--- 02-spring/src/test/java/core/spring/scope/SingletonWithPrototypeBeanTest.java
+++ 02-spring/src/test/java/core/spring/scope/SingletonWithPrototypeBeanTest.java
@@ -4,15 +4,77 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * <h1>Spring scope -  singleton Bean과 prototype Bean을 함께 사용 시, 문제점 </h1>
+ *
  * @- Singleton Bean에서 prototype Bean을 사용하는 경우,
  * prototype Beand은 이미 과거에 주입이 끝난 빈이다.
  * 따라서, Spring Container Bean 주입 시점에, 주입된 prototype bean은 spring container에 의해 신규 생성된 이후, 신규 생성되지 않.는.다.`
  */
 public class SingletonWithPrototypeBeanTest {
+
+  @Test
+  @DisplayName("Singleton Bean에 주입받은 Prototype - 의도대로 작동")
+  public void intentionalPrototypeInSingleton() {
+
+
+    // Given
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
+      SingletonWithPrototypeBean.class,
+      PrototypeBean.class
+      // ...
+      // ...
+      // SingletonWithPrototypeBean은 PrototypeBean을 주입받기 때문에, Context 내에 포함시켜야 함
+      // SingleTon Bean에 주입받을 Bean 있다면, 추가하시라.
+      // ...
+      // ...
+    );
+
+    SingletonWithPrototypeBean singletonBean1 = ac.getBean(SingletonWithPrototypeBean.class);
+    SingletonWithPrototypeBean singletonBean2 = ac.getBean(SingletonWithPrototypeBean.class);
+
+
+    PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+    int initCount = prototypeBean.getCount();
+    int intendedCount = initCount + 1;
+
+
+    // When - 최초 변경
+    int firstChange = singletonBean1.intentionalLogic();
+
+    // Then
+    assertThat(firstChange).isEqualTo(intendedCount);
+
+    List<PrototypeBean> prototypeBeans = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      // When - N 번째 변경
+      int count = singletonBean2.intentionalLogic();
+
+      // Then
+      assertThat(count).isEqualTo(intendedCount);
+
+      // When - Prototype bean 검증
+      PrototypeBean currentPrototypeBean = singletonBean2.getPrototypeBeanByObjectProvider();
+      prototypeBeans.add(currentPrototypeBean);
+
+      int beforeBeanIndex = i - 1;
+      if (i != 0) {
+        PrototypeBean beforePrototypeBean = prototypeBeans.get(beforeBeanIndex);
+        System.out.println("----- intentionalPrototypeInSingleton >  beforePrototypeBean = " + beforePrototypeBean);
+        System.out.println("----- intentionalPrototypeInSingleton >  currentPrototypeBean = " + currentPrototypeBean);
+        assertThat(currentPrototypeBean).isNotSameAs(beforePrototypeBean);
+      }
+    }
+
+  }
+
+
   @Test
   @DisplayName("Singleton Bean에 주입받은 Prototype - 의도대로 작동하지 않는 경우")
   public void unintentionalPrototypeInSingleton() {
@@ -21,32 +83,44 @@ public class SingletonWithPrototypeBeanTest {
     // Given
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
       SingletonWithPrototypeBean.class,
-      PrototypeBean.class  // SingletonWithPrototypeBean은 PrototypeBean을 주입받기 때문에, Context 내에 포함시켜야 함
-      // ...
-      // SingleTon Bean에 주입받을 Bean 있다면, 추가하시라.
-      // ...
+      PrototypeBean.class
     );
+
+    SingletonWithPrototypeBean singletonBean1 = ac.getBean(SingletonWithPrototypeBean.class);
+    SingletonWithPrototypeBean singletonBean2 = ac.getBean(SingletonWithPrototypeBean.class);
 
     PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
     int initCount = prototypeBean.getCount();
-    SingletonWithPrototypeBean singleTonBean1 = ac.getBean(SingletonWithPrototypeBean.class);
-    SingletonWithPrototypeBean singletonBean2 = ac.getBean(SingletonWithPrototypeBean.class);
+    int intendedCount = initCount + 1;
 
 
-
-    // When - 첫 번째 변경
-    int count1 = singleTonBean1.unintentionalLogic();
-    int intendedCount = initCount + 1 ;
+    // When - 최초 변경
+    int firstChange = singletonBean1.unintentionalLogic();
 
     // Then
-    assertThat(count1).isEqualTo(intendedCount);
-
-    // When - 두 번째 변경
-    int count2 = singletonBean2.unintentionalLogic();
-
-    assertThat(count2).isNotEqualTo(intendedCount);
+    assertThat(firstChange).isEqualTo(intendedCount);
 
 
+    List<PrototypeBean> prototypeBeans = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      // When - N 번째 변경
+      int count = singletonBean2.unintentionalLogic();
+
+      // Then
+      assertThat(count).isNotEqualTo(intendedCount);
+
+      // When - Prototype bean 검증
+      PrototypeBean currentPrototypeBean = singletonBean2.getPrototypeBean();
+      prototypeBeans.add(currentPrototypeBean);
+
+      int beforeBeanIndex = i - 1;
+      if (i != 0) {
+        PrototypeBean beforePrototypeBean = prototypeBeans.get(beforeBeanIndex);
+        System.out.println("----- unintentionalPrototypeInSingleton >  beforePrototypeBean = " + beforePrototypeBean);
+        System.out.println("----- unintentionalPrototypeInSingleton >  currentPrototypeBean = " + currentPrototypeBean);
+        assertThat(currentPrototypeBean).isSameAs(beforePrototypeBean);
+      }
+    }
 
   }
 
@@ -62,33 +136,50 @@ public class SingletonWithPrototypeBeanTest {
       PrototypeBean.class
     );
 
-    PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
-    int initCount = prototypeBean.getCount();
-    SingletonWithPrototypeBean singleTonBean1 = ac.getBean(SingletonWithPrototypeBean.class);
+    SingletonWithPrototypeBean singletonBean1 = ac.getBean(SingletonWithPrototypeBean.class);
     SingletonWithPrototypeBean singletonBean2 = ac.getBean(SingletonWithPrototypeBean.class);
 
+    PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+    int initCount = prototypeBean.getCount();
+    int intendedCount = initCount + 1;
 
 
     // When - 첫 번째 변경
-    int count1 = singleTonBean1.intentionalLogicButBadCode(ac);
-    int intendedCount = initCount + 1 ;
+    Map<String, Object> resultMap1 = singletonBean1.intentionalLogicButBadCode();
+
+    int firstChangedCount = (int) resultMap1.get("count");
 
     // Then
-    assertThat(count1).isEqualTo(intendedCount);
-
-    // When - 두 번째 변경
-    int count2 = singletonBean2.intentionalLogicButBadCode(ac);
-
-    assertThat(count2).isEqualTo(intendedCount);
+    assertThat(firstChangedCount).isEqualTo(intendedCount);
 
 
 
+    List<PrototypeBean> prototypeBeans = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      // When - N 번째 변경
+      Map<String, Object> resultMap2 = singletonBean2.intentionalLogicButBadCode();
+      int count = (int) resultMap2.get("count");
+      // Then
+      assertThat(count).isEqualTo(intendedCount);
+
+      // When - Prototype bean 검증
+      PrototypeBean currentPrototypeBean = (PrototypeBean) resultMap2.get("bean");
+      prototypeBeans.add(currentPrototypeBean);
+
+      int beforeBeanIndex = i - 1;
+      if (i != 0) {
+        PrototypeBean beforePrototypeBean = prototypeBeans.get(beforeBeanIndex);
+        System.out.println("----- intentionalPrototypeInSingleton_butBadArchitecture > beforePrototypeBean = " + beforePrototypeBean);
+        System.out.println("----- intentionalPrototypeInSingleton_butBadArchitecture > currentPrototypeBean = " + currentPrototypeBean);
+        assertThat(currentPrototypeBean).isNotSameAs(beforePrototypeBean);
+      }
+    }
 
   }
 
 
   @Test
-  @DisplayName("Prototype 단독으로 사용되는 경우")
+  @DisplayName("Prototype - 단독 사용")
   public void prototypeWithoutSingleton() throws Exception {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
 
@@ -100,14 +191,11 @@ public class SingletonWithPrototypeBeanTest {
     assertThat(prototypeBean1.getCount()).isEqualTo(1);
 
 
-
-
     // Bean 2
     PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
     System.out.println("prototypeBean2 = " + prototypeBean2);
     prototypeBean2.addCount();
     assertThat(prototypeBean2.getCount()).isEqualTo(1);
-
 
 
     // Then
