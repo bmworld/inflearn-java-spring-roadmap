0367ac91514e1d90865736691d02cc89ec9328bb
sbkim <victory289@naver.com>
2023-06-27 23:36:13 +0900

    03-MVC-servlet- Servlet 기본 동작 실습 (request, response)

--- 03-mvc/01-servlet/src/main/java/com/example/servlet/ServletApplication.java
+++ 03-mvc/01-servlet/src/main/java/com/example/servlet/ServletApplication.java
@@ -2,7 +2,10 @@ package com.example.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+
+@ServletComponentScan // Servlet 자동 등록
 @SpringBootApplication
 public class ServletApplication {
 
