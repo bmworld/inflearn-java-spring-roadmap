047c8d2bdf6f3104160d48c5c5e4f602e4289e8c
sbkim <victory289@naver.com>
2023-07-23 22:24:19 +0900

    04-Spring MVC-advanced- Message 국제화 - 삽질.........과 성공
    * Message 국제화 사용 시, System Default Language 매우 중요
      - 강사님: Korean
      - 본인: English
    
      1. application.properties > spring.messages.basename=messages 설정한 경우, messages.properties 파일존재해야함. (없을 경우, default locale null설정 시, 에러발생)
    
     2. messages.properties > 시스템 기본 언어(본인: en)에 해당하는 properties OVERRIDE 된다.
       => [중요] messages.properties에 무슨 값을 넣든 상관없이, messages_en.properties 값이 사용된다.

--- 04-mvc-advanced/message/src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ 04-mvc-advanced/message/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -1,6 +1,5 @@
 package hello.itemservice.message;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,46 +8,77 @@ import org.springframework.context.NoSuchMessageException;
 
 import java.util.Locale;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+
+/**
+ *
+ *  <h1>[주의 사항] -_-...</h1>
+ *  <p>- 본인 SYSTEM DEFAULT 언어: English
+ *  <pre>
+ *    1. application.properties > spring.messages.basename=messages 설정한 경우, messages.properties 파일존재해야함.
+ *    2. messages.properties > 시스템 기본 언어(본인: en)에 해당하는 properties OVERRIDE 된다.
+ *    <h1>[OVERRIDE] messages.properties => messages_en.properties </h1>
+ *
+ *  </pre>
+ *
+ */
 @SpringBootTest
 public class MessageSourceTest {
 
-    @Autowired
-    MessageSource ms;
+  @Autowired
+  private MessageSource ms;
 
-    @Test
-    void helloMessage() {
-        String result = ms.getMessage("hello", null, null);
-        assertThat(result).isEqualTo("안녕");
-    }
+  @Test
+  public void helloMessage() {
 
-    @Test
-    void notFoundMessageCode() {
-        assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
-                .isInstanceOf(NoSuchMessageException.class);
-    }
+    String result = ms.getMessage("hello", null, null); // Locale 넣지않으면 default 또는 System Default language 값 가져옴
+    System.out.println("result = " + result);
+    assertThat(result).isEqualTo("hello");
+  }
 
-    @Test
-    void notFoundMessageCodeDefaultMessage() {
-        String result = ms.getMessage("no_code", null, "기본 메시지", null);
-        assertThat(result).isEqualTo("기본 메시지");
-    }
+  @Test
+  public void notFoundMessageCode() {
+    assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
+      .isInstanceOf(NoSuchMessageException.class);
+  }
 
-    @Test
-    void argumentMessage() {
-        String message = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
-        assertThat(message).isEqualTo("안녕 Spring");
-    }
+  @Test
+  public void notFoundMessageCodeDefaultMessage() {
+    String defaultMessage = "기본 메시지";
+    String result = ms.getMessage("no_code", null, defaultMessage, null);
+    assertThat(result).isEqualTo(defaultMessage);
+  }
 
-    @Test
-    void defaultLang() {
-        assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕");
-        assertThat(ms.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
-    }
+  @Test
+  public void argumentMessageByDefaultLang() {
+    String message = ms.getMessage("hello.name", new Object[]{"Spring", "JAVA"}, null);
+    System.out.println("message = " + message);
+    assertThat(message).isEqualTo("hello Spring JAVA");
+  }
 
-    @Test
-    void enLang() {
-        assertThat(ms.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
-    }
+  @Test
+  public void defaultLang() {
+    String defaultLangResult = ms.getMessage("hello", null, null);
+    System.out.println("defaultLangResult = " + defaultLangResult);
+    assertThat(defaultLangResult).isEqualTo("hello");
+  }
+
+  @Test
+  public void enLang() {
+    String result = ms.getMessage("hello", null, Locale.ENGLISH);
+    System.out.println("result = " + result);
+
+    assertThat(result).isEqualTo("hello");
+  }
+
+
+  @Test
+  public void koLang() {
+    String result = ms.getMessage("hello", null, Locale.KOREAN);
+    System.out.println("result = " + result);
+
+    assertThat(result).isEqualTo("안녕");
+  }
 }
