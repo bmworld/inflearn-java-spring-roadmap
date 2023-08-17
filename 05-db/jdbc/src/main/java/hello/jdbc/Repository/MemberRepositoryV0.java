2358880eb63a2e3ad17caba4e9039a5d4f340297
sbkim <victory289@naver.com>
2023-08-17 23:58:01 +0900

    05-Spring DB: JDBC 개발 - 조회, 수정

--- 05-db/jdbc/src/main/java/hello/jdbc/Repository/MemberRepositoryV0.java
+++ 05-db/jdbc/src/main/java/hello/jdbc/Repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * <h1>JDBC - DriverManager 사용</h1>
@@ -20,9 +21,115 @@ import java.sql.*;
  */
 @Slf4j
 public class MemberRepositoryV0 {
+
+  public void update(String memberId, int money) throws SQLException {
+    String sql = "UPDATE member SET money=? WHERE member_id=?";
+
+    Connection con = null;
+    PreparedStatement pstmt = null;
+
+
+    try {
+      con = getConnection();
+      pstmt = con.prepareStatement(sql);
+      pstmt.setInt(1, money);
+      pstmt.setString(2, memberId);
+      int resultSize = pstmt.executeUpdate(); // Query 실행으로 영향받은 `row` count 반환.
+
+      log.info("update > resultSize = {}", resultSize);
+
+    } catch (SQLException e) {
+      log.error("---- update > db error", e);
+      e.getStackTrace();
+      throw e;
+    } finally {
+      close(con, pstmt, null);
+    }
+  }
+
+
+  public void deleteAll() throws SQLException {
+
+    String sql = "DELETE FROM Member";
+
+    Connection con = null;
+    PreparedStatement pstmt = null;
+
+    try {
+      con = getConnection();
+      pstmt = con.prepareStatement(sql);
+      pstmt.executeUpdate();
+
+    } catch (Exception e) {
+      log.error("---- deleteAll > db error", e);
+      throw  e;
+    } finally {
+      close(con, pstmt, null);
+    }
+
+  }
+
+
+  /**
+   * <h1>ResultSet</h1>
+   * <pre>
+   *   - select query 날렸을 때 보이는 그 테이블 구조랑 똑같음
+   *   - ResultSet 내부 `cursor`를 이동하여, 다음 데이터를 조회할 수 있다.
+   *   - `rs.next()`: cursor가 다음 row로 이동,
+   *     -> 최초의 cursor는 DATA를 가르키고 있지 않음.
+   *     -> 따라서, 데티어 조회를 위해 최초 rs.next() 최초 한번 호출 필요.
+   *
+   *   - rs.getString("..."): 현재 cursor가 위치한 row의 column 데이터를  `String Type`으로 반환.
+   *   - rs.getInt("..."): 현재 cursor가 위치한 row의 column 데이터를 `int Type`으로 반환.
+   *
+   *   - rs.next() -> 데이터가 없는 경우, `false` 반환.
+   *
+   * </pre>
+   */
+  public Member findById(String memberId) throws SQLException {
+    String sql = "SELECT * FROM Member WHERE member_id = ?";
+
+    Connection con = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+
+    try {
+      con = getConnection();
+      pstmt = con.prepareStatement(sql);
+      pstmt.setString(1, memberId);
+
+      rs = pstmt.executeQuery();
+
+
+      System.out.println("--- rs = " + rs);
+
+
+      // rs.next() 를 한번은 실행해줘야, 실제 데이터에 있는 것부터 실행이 된다.
+      if (rs.next()) {
+        Member member = new Member();
+        member.setMemberId(rs.getString("member_id"));
+        member.setMoney(rs.getInt("money"));
+        return member;
+      } else {
+        // resultSet에서 `커서`가 있는데... 거기에서 데이터가 없는 경우.
+
+        throw new NoSuchElementException("Member not found memberId=" + memberId);
+      }
+
+    } catch (Exception e) {
+      log.error("---- findById > db error", e);
+      throw  e;
+    } finally {
+      close(con, pstmt, rs);
+    }
+  }
+
+
+
+
   public Member save(Member member) throws SQLException {
 
-    String sql = "insert into MEMBER(member_id, money) values (?,?)";
+    String sql = "INSERT INTO MEMBER(member_id, money) VALUES (?,?)";
 
     Connection con = null;
     // PreparedStatement: Parameter Binding 가능한 sql
@@ -56,6 +163,15 @@ public class MemberRepositoryV0 {
     }
   }
 
+
+
+
+  // =================================================================================================
+  // =================================================================================================
+  // =================================================================================================
+  // =================================================================================================
+  // =================================================================================================
+
   private void close (Connection con, Statement stmt, ResultSet rs) {
     if (rs != null) {
       try {
@@ -86,6 +202,7 @@ public class MemberRepositoryV0 {
 
 
   }
+
 
   private Connection getConnection() {
     return DBConnectionUtils.getConnection();
--- 05-db/jdbc/src/test/java/hello/jdbc/Repository/MemberRepositoryV0Test.java
+++ 05-db/jdbc/src/test/java/hello/jdbc/Repository/MemberRepositoryV0Test.java
@@ -1,21 +1,86 @@
 package hello.jdbc.Repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
   private MemberRepositoryV0 repository = new MemberRepositoryV0();
 
+  @BeforeEach
+  private void init() throws SQLException {
+    repository.deleteAll();
+  }
+
   @Test
   void save() throws SQLException {
+    String memberId = createMemberId();
+    Member member = generateMember(memberId, 10000);
 
-    Member member = new Member("memberV0", 10000);
+    assertThat(member.getMemberId()).isEqualTo(memberId);
+  }
+
+  @Test
+  @DisplayName("findMemberById")
+  public void findMemberById() throws Exception {
+
+    // Given
+    Member member = generateMember();
+    // when
+    Member foundMember = repository.findById(member.getMemberId());
+    log.info("foundMember={}", foundMember);
+    log.info("member == foundMember => {}", (member == foundMember)); // false => instances 다름
+    log.info("member.equals(foundMember) => {}", (member.equals(foundMember))); // true => equalsAndHashCode 비교는 동일
+
+    // Then
+    assertThat(foundMember).isEqualTo(member);
+
+
+  }
+
+
+  @Test
+  @DisplayName("update")
+  public void update() throws Exception {
+    // Given
+    int beforeMoney = 10000;
+    int afterMoney = 20000;
+    Member member = generateMember(beforeMoney);
+    // When
+    repository.update(member.getMemberId(), afterMoney);
+    // Then
+
+    Member foundMember = repository.findById(member.getMemberId());
+    assertThat(foundMember.getMoney()).isEqualTo(afterMoney);
+
+  }
+
+  private Member generateMember(String memberId, int money) throws SQLException {
+    Member member = new Member(memberId, money);
     repository.save(member);
+    return member;
+  }
 
+  private Member generateMember(int money) throws SQLException {
+    return generateMember(createMemberId(), money);
+  }
+
+  private Member generateMember() throws SQLException {
+    return generateMember(10000);
+  }
+
+
+  private static String createMemberId() {
+    String uuid = UUID.randomUUID().toString();
+    return "member-" + uuid.substring(0, 3);
   }
 }
