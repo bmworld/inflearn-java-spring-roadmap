5b372b25ee065748287435bed6d2c827574a30da
sbkim <victory289@naver.com>
2023-09-16 21:48:45 +0900

    06-Spring DB-advanced: SimpleJdbcInsert

--- 06-db-advanced/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ 06-db-advanced/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -11,7 +11,8 @@ import org.springframework.context.annotation.Profile;
 
 //@Import(MemoryConfig.class) // Memory Ver.
 //@Import(JdbcTemplateV1Config.class)
-@Import(JdbcTemplateV2Config.class)
+//@Import(JdbcTemplateV2Config.class)
+@Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")  // * 해당 Package만 Component Scan / 나머지는 수동 등록해야함
 public class ItemServiceApplication {
 
