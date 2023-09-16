0a177bd95483b7fd71ca12405ec5a7d71a0b2f95
sbkim <victory289@naver.com>
2023-09-16 22:24:18 +0900

    06-Spring DB-advanced: TEST 환경 격리화 방법 (TEST 전용 DB사용 & `rollback` by transaction manager)

--- 06-db-advanced/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ 06-db-advanced/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -5,88 +5,118 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+/**
+ * <h1>TEST 주요 원칙</h1>
+ * <pre>
+ *   1. 각 테스트는 다른 테스트와 격리된 환경이다.
+ *   2. 테스트는 반복해서 실행할 수 있다.
+ * </pre>
+ */
+@SpringBootTest // => 상위 패키지를 탐색하여 `@SpringBootApplication`을 찾는다. => 발견한 Application내의 설정들도 모두 사용한다. (ex. @Import(JdbcTemplateV3Config.class))
 class ItemRepositoryTest {
 
-    @Autowired
-    ItemRepository itemRepository;
+  @Autowired
+  private PlatformTransactionManager tm;
+  private TransactionStatus status;
 
-    @AfterEach
-    void afterEach() {
-        //MemoryItemRepository 의 경우 제한적으로 사용
-        if (itemRepository instanceof MemoryItemRepository) {
-            itemRepository.deleteAll();
-        }
+  @Autowired
+  private ItemRepository itemRepository;
+
+  @BeforeEach
+  void beforeEach() {
+    status = tm.getTransaction(new DefaultTransactionDefinition());
+  }
+
+  @AfterEach
+  void afterEach() {
+    //MemoryItemRepository 의 경우 제한적으로 사용
+    if (itemRepository instanceof MemoryItemRepository) {
+      itemRepository.deleteAll();
     }
 
-    @Test
-    void save() {
-        //given
-        Item item = new Item("itemA", 10000, 10);
 
-        //when
-        Item savedItem = itemRepository.save(item);
+    // DB 격리화방법: transaction 시작 시, 초기 status 적용하여, rollback
+    tm.rollback(status);
 
-        //then
-        Item findItem = itemRepository.findById(item.getId()).get();
-        assertThat(findItem).isEqualTo(savedItem);
-    }
+  }
 
-    @Test
-    void updateItem() {
-        //given
-        Item item = new Item("item1", 10000, 10);
-        Item savedItem = itemRepository.save(item);
-        Long itemId = savedItem.getId();
 
-        //when
-        ItemUpdateDto updateParam = new ItemUpdateDto("item2", 20000, 30);
-        itemRepository.update(itemId, updateParam);
+  @Test
+  void save() {
+    //given
+    Item item = new Item("itemA", 10000, 10);
 
-        //then
-        Item findItem = itemRepository.findById(itemId).get();
-        assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
-        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
-        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
-    }
+    //when
+    Item savedItem = itemRepository.save(item);
 
-    @Test
-    void findItems() {
-        //given
-        Item item1 = new Item("itemA-1", 10000, 10);
-        Item item2 = new Item("itemA-2", 20000, 20);
-        Item item3 = new Item("itemB-1", 30000, 30);
+    //then
+    Item findItem = itemRepository.findById(item.getId()).get();
+    assertThat(findItem).isEqualTo(savedItem);
+  }
 
-        itemRepository.save(item1);
-        itemRepository.save(item2);
-        itemRepository.save(item3);
+  @Test
+  void updateItem() {
+    //given
+    Item item = new Item("item1", 10000, 10);
+    Item savedItem = itemRepository.save(item);
+    Long itemId = savedItem.getId();
 
-        //둘 다 없음 검증
-        test(null, null, item1, item2, item3);
-        test("", null, item1, item2, item3);
+    //when
+    ItemUpdateDto updateParam = new ItemUpdateDto("item2", 20000, 30);
+    itemRepository.update(itemId, updateParam);
 
-        //itemName 검증
-        test("itemA", null, item1, item2);
-        test("temA", null, item1, item2);
-        test("itemB", null, item3);
+    //then
+    Item findItem = itemRepository.findById(itemId).get();
+    assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+    assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+    assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
+  }
 
-        //maxPrice 검증
-        test(null, 10000, item1);
+  @Test
+  void findItems() {
+    itemRepository.deleteAll();
+    //given
+    Item item1 = new Item("itemA-1", 10000, 10);
+    Item item2 = new Item("itemA-2", 20000, 20);
+    Item item3 = new Item("itemB-1", 30000, 30);
 
-        // 둘 다 있음 검증
-        test("itemA", 10000, item1);
-    }
+    itemRepository.save(item1);
+    itemRepository.save(item2);
+    itemRepository.save(item3);
 
-    void test(String itemName, Integer maxPrice, Item... items) {
-        List<Item> result = itemRepository.findAll(new ItemSearchCond(itemName, maxPrice));
-        assertThat(result).containsExactly(items);
-    }
+    //둘 다 없음 검증
+    test(null, null, item1, item2, item3);
+    test("", null, item1, item2, item3);
+
+    //itemName 검증
+    test("itemA", null, item1, item2);
+    test("temA", null, item1, item2);
+    test("itemB", null, item3);
+
+    //maxPrice 검증
+    test(null, 10000, item1);
+
+    // 둘 다 있음 검증
+    test("itemA", 10000, item1);
+  }
+
+  void test(String itemName, Integer maxPrice, Item... items) {
+    List<Item> result = itemRepository.findAll(new ItemSearchCond(itemName, maxPrice));
+    assertThat(result).containsExactly(items);
+  }
 }
