00a6827234b3b017fb5773724602ae879d234663
sbkim <victory289@naver.com>
2023-10-09 00:24:40 +0900

    07-Spring-advanced: AutoProxyCreator 자동 프록시 생성기 - advanced (AspectJ 제공 PointCut 사용 : AOP 대상을 정밀하게 선택하기 위함)
    * 이슈: 기존 Advisor의 경우, 예상치 못한 Config내에 "order**" 등의 이름을 가진 Bean에도 Proxy가 적용되었다.
    => 위 이슈를 해결하기 위해,
      AspectJ가 제공하는 pointCut을 사용하여 적용 대상 Bean을 정밀하게 지정한다.
    
    * AspectJ Expression 문법 설명
    - `*`: 모든 반환 타입
    - `hello.proxy.app..`: 대상 패키지 및 그 하위 패키지
    - `*(..)`: `*` 모든 메서드 이름 / `(..)` parameter 는 no상관
    
    - `&&`: and 조건 추가
    - `!execution(* hello.proxy.app..noLog(..))`: 해당 메서드 중, noLog 메서드 제외 (+해당 parameter는 no상관)

--- 07-spring-advanced/proxy/src/main/java/hello/proxy/config/v5_autoProxy/AutoProxyConfig.java
+++ 07-spring-advanced/proxy/src/main/java/hello/proxy/config/v5_autoProxy/AutoProxyConfig.java
@@ -6,6 +6,7 @@ import hello.proxy.config.v3_proxyFactory.advice.LogTraceAdvice;
 import hello.proxy.trace.logtrace.LogTrace;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -24,11 +25,64 @@ import org.springframework.context.annotation.Import;
 @Import({AppV1Config.class, AppV2Config.class}) // 예제에서 이미 등록한 interface Class & Concrete Class 을 추가한 Config를 가져온다.
 public class AutoProxyConfig {
 
+  /**
+   * <h1>해당 advisor 사용 시, setMappedNames 내에 포함되는 AppConfig에 설정된 Bean까지 등록되는 ISSUE가 발생한다.  </h1>
+   */
+//  @Bean
+//  public Advisor advisor1(LogTrace logTrace) {
+//    // pointcut
+//    NameMatchMethodPointcut pointCut = new NameMatchMethodPointcut();
+//    pointCut.setMappedNames("request*", "order*", "save*"); // 적용대상 method 패턴을 지정할 수 있다.
+//    // advice
+//    LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+//    return new DefaultPointcutAdvisor(pointCut, advice);
+//  }
+
+
+  /**
+   * <h1>[실무용] AspectJ라는 AOP에 특화된 PointCut 표현식을 적용한다.</h1>
+   * <pre>
+   *   목적: 예상치 못한 Bean에 AOP 적용 방지 => 정밀하게 AOP 대상 Method 지정
+   * </pre>
+   * <h1>해당 문법의 ISSUE: no-log인 메서드에도 적용됨</h1>
+   */
+//  @Bean
+//  public Advisor advisor2(LogTrace logTrace) {
+//    // pointcut => AspectJ
+//    AspectJExpressionPointcut pointCut = new AspectJExpressionPointcut();
+//    pointCut.setExpression("execution(* hello.proxy.app..*(..))");
+//    /**
+//     * * AspectJ Expression 문법 설명
+//     * - `*`: 모든 반환 타입
+//     * - `hello.proxy.app..`: 대상 패키지 및 그 하위 패키지
+//     * - `*(..)`: `*` 모든 메서드 이름 / `(..)` parameter 는 상관없다는 의미
+//     */
+//
+//
+//
+//    // advice
+//    LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+//    return new DefaultPointcutAdvisor(pointCut, advice);
+//  }
+
+
+  /**
+   * <h1>Advanced Example: `no-log` 메서드는 AOP 대상에서 제외</h1>
+   */
   @Bean
-  public Advisor advisor1(LogTrace logTrace) {
-    // pointcut
-    NameMatchMethodPointcut pointCut = new NameMatchMethodPointcut();
-    pointCut.setMappedNames("request*", "order*", "save*"); // 적용대상 method 패턴을 지정할 수 있다.
+  public Advisor advisor3(LogTrace logTrace) {
+    // pointcut => AspectJ
+    AspectJExpressionPointcut pointCut = new AspectJExpressionPointcut();
+    pointCut.setExpression("execution(* hello.proxy.app..*(..)) && !execution(* hello.proxy.app..noLog(..))");
+    /**
+     * * AspectJ Expression 문법 설명
+     * - `*`: 모든 반환 타입
+     * - `hello.proxy.app..`: 대상 패키지 및 그 하위 패키지
+     * - `*(..)`: `*` 모든 메서드 이름 / `(..)` parameter 는 상관없다는 의미
+     */
+
+
+
     // advice
     LogTraceAdvice advice = new LogTraceAdvice(logTrace);
     return new DefaultPointcutAdvisor(pointCut, advice);
