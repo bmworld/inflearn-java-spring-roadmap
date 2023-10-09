823c2d07a92f8a77a9c47e1e87cbeb6e79e30f34
sbkim <victory289@naver.com>
2023-10-09 21:05:20 +0900

    07-Spring-advanced: AOP - 적용 (ex. Transaction `Advice`)

--- 07-spring-advanced/aop/src/test/java/hello/aop/AopTest.java
+++ 07-spring-advanced/aop/src/test/java/hello/aop/AopTest.java
@@ -5,6 +5,7 @@ import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
 import hello.aop.order.aop.AspectV1;
 import hello.aop.order.aop.AspectV2;
+import hello.aop.order.aop.AspectV3;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -17,7 +18,8 @@ import org.springframework.context.annotation.Import;
 @Slf4j
 @SpringBootTest
 //@Import(AspectV1.class)
-@Import(AspectV2.class)
+//@Import(AspectV2.class)
+@Import(AspectV3.class)
 public class AopTest {
   @Autowired
   private OrderService orderService;
@@ -25,14 +27,14 @@ public class AopTest {
   @Autowired
   private OrderRepository orderRepository;
 
-  @DisplayName("app 적용여부 조회")
+  @DisplayName("Aop 적용여부 조회")
   @Test
   void getAopInfo() {
     log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderRepository));
     log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderService));
   }
 
-  @DisplayName("Aop 적용여부 확인: Success CASE")
+  @DisplayName("Aop 적용여부 확인: Success")
   @Test
   void successCASE () {
     orderService.orderItem("itemA");
@@ -40,7 +42,7 @@ public class AopTest {
   }
 
 
-  @DisplayName("Exception CASE")
+  @DisplayName("Aop 적용여부 확인 Exception")
   @Test
   void exceptionCASE () {
     Assertions.assertThatThrownBy(() -> orderService.orderItem(OrderConst.exceptionItemName))
