9103ba46b2fc58d7ffebf42300e2a3f9b72403e9
sbkim <victory289@naver.com>
2024-07-06 20:01:21 +0900

    08-springboot: `@ConditionalOnProperty`
     # `@ConditionalOnProperty`: 특정조건이 만족할 때 실행되도록 함
     ㄴ`name` = 환경정보의 property 이름 / havingValue = 해당 name 값의 value가 지정한 값인지.
     ㄴ 환경정보: application.yml (memory=on) 또는 IDE - Configuration - VM Option (`-Dmemory`=on)

--- 08-springboot/autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ 08-springboot/autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -1,14 +1,18 @@
 package hello.config;
 
-import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Conditional(MemoryCondition.class) // 해당 Condition 이 True 인 경우에만 아래 Configuration 이 Bean 등록됨.
+// @Conditional(MemoryCondition.class) // 해당 Condition 이 True 인 경우에만 아래 Configuration 이 Bean 등록됨.
+// `@ConditionalOnProperty`: 특정조건이 만족할 때 실행함
+// `name` = 환경정보의 property 이름 / havingValue = 해당 name 값의 value가 지정한
+// 값인지.
+// * 환경정보: application.yml (memory=on) 또는 IDE - Configuration - VM Option (`-Dmemory`=on)
+@ConditionalOnProperty(name = "memory", havingValue = "on")
 public class MemoryConfig {
 
   @Bean
